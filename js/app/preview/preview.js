a918e19453f617c364efe32d2d4212cf0e8b61b2
hejiao76 <380791842@qq.com>
2016-12-03 22:38:07 +0800

    新增代码

--- js/app/preview/preview.js
+++ js/app/preview/preview.js
@@ -39,11 +39,29 @@ define(['common/render', 'app/baseURL', 'baseCookie', 'app/baseFinal','common/ut
                    var data=response.data;
                    if(data){
                        renderDefaultTmpl(data);
+                       tmpSaveImgData(data.images);
                    }
                }
             }
         });
     };
+    var tmpSaveImgData = function (images){
+        if(images.length>0){
+            var tmp_img_url_array=[];
+            var tmp_img_title_array=[];
+            for(var i=0;i<images.length;i++){
+                tmp_img_url_array.push(images[i].img);
+                tmp_img_title_array.push(images[i].title);
+            };
+            localStorage.setItem("tmp_img_url",tmp_img_url_array.join("<%%>"));
+            localStorage.setItem("tmp_img_title",tmp_img_title_array.join("<%%>"));
+
+        }else{
+            localStorage.removeItem("tmp_img_url");
+            localStorage.removeItem("tmp_img_title");
+        }
+
+    }
     var loadMusic = function (data){
         var locationMusicSrc = localStorage.getItem("musicSrc");
         if(locationMusicSrc){
