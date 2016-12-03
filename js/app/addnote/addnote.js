b627678c4a2da916539092cec3235bb541697c91
jingxy <953885034@qq.com>
2016-12-03 22:29:49 +0800

    添加图片

--- js/app/addnote/addnote.js
+++ js/app/addnote/addnote.js
@@ -15,6 +15,8 @@ define(['common/render', 'app/baseURL', 'baseCookie', 'app/baseFinal','common/aj
         $(document).off('click','.deleImg').on('click','.deleImg',deleImg);
         $(document).off('click','.btn_true').on('click','.btn_true',setPhoto);
         $(document).off('click','.btn_false').on('click','.btn_false',backUrl);
+        $(document).off('click','.btn_prev').on('click','.btn_prev',thisprev);
+        $(document).off('click','.btn_next').on('click','.btn_next',thisnext);
         $(document).on('change','#addImg',function(){
             $.ajaxFileUpload({
                 url: URL.baseURLForward1 + 'api/images/upload',
@@ -161,6 +163,35 @@ define(['common/render', 'app/baseURL', 'baseCookie', 'app/baseFinal','common/aj
             };
         };
     }
+    var thisprev = function(){
+        var index=parseInt($(this).parents(".photo_here").index())-1;
+        var oldarr = localStorage.getItem('phoneList').split(',');
+        if(index>0){
+                var temp1 = oldarr[index];
+                var temp2 = oldarr[index-1];
+
+                oldarr[index] = temp2;
+                oldarr[index-1] = temp1;
+            localStorage.setItem('phoneList',oldarr);
+            renderContainer(oldarr);
+            pubu();
+        }
+
+    };
+    var thisnext = function(){
+        var index=parseInt($(this).parents(".photo_here").index())-1;
+        var oldarr = localStorage.getItem('phoneList').split(',');
+        if(index<oldarr.length-1){
+            var temp1 = oldarr[index];
+            var temp2 = oldarr[index+1];
+
+            oldarr[index] = temp2;
+            oldarr[index+1] = temp1;
+            localStorage.setItem('phoneList',oldarr);
+            renderContainer(oldarr);
+            pubu();
+        }
+    };
     return {
         init:init
     }
